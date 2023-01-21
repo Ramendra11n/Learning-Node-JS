f0a55eefa8d8229e9e37ef0946ac74392d90a65e
ramendra mishra <76106853+Ramendra11n@users.noreply.github.com>
2023-01-21 16:08:06 +0530

    Add files via upload

---
+++ creatingapi.js
@@ -0,0 +1,14 @@
+// const http=require('http');
+// const data=require('./data')
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'application/json'})
+//     res.write(JSON.stringify(data))
+//     res.end();
+// }).listen(8000)
+
+//the argv function takes the input given at the time of the
+//running as its parameters can access them using the argv[2]...argv[n]
+console.log(process.argv[2]);
+
+
+
